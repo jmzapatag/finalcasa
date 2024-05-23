8b9d7c0c603e2907baa8a5ee93dec8785645fe89
jmzapatag <69641302+jmzapatag@users.noreply.github.com>
2024-05-23 01:54:50 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
+from PIL import Image
 
 st.title("Cocina Inteligente")
 st.subheader("Página por Jorge, Kely y Juan <3")
+image = Image.open('cocina.jpg')
+
+st.image(image)
