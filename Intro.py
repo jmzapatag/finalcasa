12c3f5699b65219a9e0cf8c527ae6eedd17cee24
jmzapatag <69641302+jmzapatag@users.noreply.github.com>
2024-05-23 07:37:38 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -2,7 +2,8 @@ import streamlit as st
 from PIL import Image
 
 st.title("Cocina Inteligente")
-st.subheader("Página por Jorge, Kely y Juan <3")
+st.subheader("Maneja aqui tu cocina inteligente desde tu dispositivo preferido")
+st.write('Página por Jorge, Kely y Juan <3')
 image = Image.open('cocina.jpg')
 
 st.image(image)
